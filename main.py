2122ae00553e368236dfb7e72b62a66ecf145c6c
Oskar Weber <s189424@student.pg.edu.pl>
2022-12-17 23:37:01 +0100

    comment

--- main.py
+++ main.py
@@ -22,6 +22,22 @@ class Plots:
         self.error_box = self.fig.text(0.025, 0.025, '', fontsize=11) #Object displaying feedback message
         self.elements = {} #Dict holding references to the elements on the plot
 
+    @staticmethod
+    def errorBoxFailure(error_box, message: str):
+        error_box.set_text(message)
+        error_box.set_color('red')
+        error_box.set_bbox(dict(facecolor='none', edgecolor='red'))
+
+    @staticmethod
+    def errorBoxSuccess(error_box, message: str):
+        error_box.set_text(message)
+        error_box.set_color('black')
+        error_box.set_bbox(dict(facecolor='none', edgecolor='none'))
+
+    @staticmethod
+    def s_round(number, base=5) -> float:
+        return base * np.round(number / base)
+
     def createTextbox(self, element_name: str, title: str, initial_text: str, bottom=0.57, width=0.12, height=0.05, left=0.02): #Adds a text box object to the plot
         box_ax = plt.axes([left, bottom, width, height])
         box_ax.text(0, 1.1, title)
@@ -67,31 +83,24 @@ class Plots:
             self.fig.savefig(file_name)
             Plots.errorBoxSuccess(self.error_box, 'Plot successfully saved.')
 
-    @staticmethod
-    def errorBoxFailure(error_box, message: str):
-        error_box.set_text(message)
-        error_box.set_color('red')
-        error_box.set_bbox(dict(facecolor='none', edgecolor='red'))
-
-    @staticmethod
-    def errorBoxSuccess(error_box, message: str):
-        error_box.set_text(message)
-        error_box.set_color('black')
-        error_box.set_bbox(dict(facecolor='none', edgecolor='none'))
-
-    @staticmethod
-    def s_round(number, base=5) -> float:
-        return base * np.round(number / base)
-
 
 class Friction(Plots):
     g = float(9.8067)
+    surface_types = ('concrete', 'asphalt', 'basalt slab', 'dirt road')
+    surface_conditions = ('dry', 'wet')
+    coefficient_values = [0.7, 0.9, 0.45, 0.75, 0.4, 0.65, 0.35, 0.55]
 
     def __init__(self, x, angle: float, coefficient: float, plot_title='Friction', x_label='time[s]', y_label='V(t)[m/s]'): #Default friction plot
         y = Friction.calculateVelocity(Friction.inclinedPlaneAcceleration(angle, coefficient), x)
         Plots.__init__(self, x, y, plot_title, x_label, y_label, line_color='green')
         self.angle = angle
         self.coefficient = coefficient
+        self.coefficient_choice = ['', '']
+
+    @staticmethod
+    def getCoefficientDict() -> dict:
+        coefficient_list = [s_condition + ' ' + s_type for s_condition in Friction.surface_conditions for s_type in Friction.surface_types]
+        return dict(zip(coefficient_list, Friction.coefficient_values))
 
     @staticmethod
     def inclinedPlaneAcceleration(angle: float, coefficient: float) -> float:
@@ -118,17 +127,30 @@ class Friction(Plots):
                                           self.x))
         assert type(self.angle) == float
 
-    def setFrictionCoefficient(self, new_friction_coefficient: float): #Updates friction coefficient
+    def setFrictionCoefficient(self, new_coefficient: float): #Updates friction coefficient
         try:
-            if not(new_friction_coefficient.isnumeric()):
+            if not(new_coefficient.isnumeric()):
                 raise TypeError('Friction coefficient must be a numeric value')
         except TypeError as err:
             Friction.errorBoxFailure(self.error_box, str(err))
         else:
-            self.coefficient = float(new_friction_coefficient)
+            self.coefficient = float(new_coefficient)
             self.setAxisY(Friction.calculateVelocity(Friction.inclinedPlaneAcceleration(self.angle, self.coefficient),
                                           self.x))
             Friction.errorBoxSuccess(self.error_box, 'Data updated')
+        assert type(self.coefficient) == float
+
+    def r_setCoefficient(self, label: str):
+        if label in Friction.surface_types:
+            self.coefficient_choice[1] = label
+        if label in Friction.surface_conditions:
+            self.coefficient_choice[0] = label
+        if self.coefficient_choice[0] != '' and self.coefficient_choice[1] != '':
+            self.coefficient = float(Friction.getCoefficientDict()[' '.join(self.coefficient_choice)])
+            self.setAxisY(Friction.calculateVelocity(Friction.inclinedPlaneAcceleration(self.angle, self.coefficient),
+                                                     self.x))
+            Friction.errorBoxSuccess(self.error_box, 'Data updated')
+
 
 if __name__ == '__main__':
     time_axis = np.arange(0, 21)
@@ -139,14 +161,16 @@ if __name__ == '__main__':
     friction_plot.createTextbox('incline_angle', 'Angle[degrees]: ', '30', 0.45)
     friction_plot.createTextbox('save_button', 'Save Plot:', 'File name', 0.35)
     #Radio buttons
-    friction_plot.createRadio('surface_radio', 'Surface type: ', 0.8, buttons=('Concrete', 'Wood'))
-    friction_plot.createRadio('condition_radio', 'Surface condition: ', 0.65, buttons=('Dry', 'Wet'))
+    friction_plot.createRadio('surface_radio', 'Surface type: ', 0.8, buttons=('concrete', 'asphalt', 'basalt slab', 'dirt road'))
+    friction_plot.createRadio('condition_radio', 'Surface condition: ', 0.65, buttons=('dry', 'wet'))
     #Buttons
 
     #Text boxes' functions
     friction_plot.elements['incline_angle'].on_submit(friction_plot.setAngle)
     friction_plot.elements['save_button'].on_submit(friction_plot.savePlot)
     #Radio buttons' functions
+    friction_plot.elements['surface_radio'].on_clicked(friction_plot.r_setCoefficient)
+    friction_plot.elements['condition_radio'].on_clicked(friction_plot.r_setCoefficient)
 
     #Buttons' functions
 
